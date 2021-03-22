5debaab1dbe3b779a540ea5754a9a6a98fd8d07a
sw0637 <sw0637_hong@naver.com>
2021-03-22 23:47:33 +0900

    add hello controller & dto & test

---
+++ src/main/java/com/hsw0637/sample/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.hsw0637.sample.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
